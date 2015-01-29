2c13d99845574036bafec5d4f2842499a37470b5
Felix0830 <ddjunshuhong@126.com>
2015-01-29 22:18:54 +0800

    完成生成密码界面

--- My Password Manage/GeneratePwd.xaml.cs
+++ My Password Manage/GeneratePwd.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace My_Password_Manage
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for GeneratePwd.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GeneratePwd : Window
     {
-        public MainWindow()
+        public GeneratePwd()
         {
             InitializeComponent();
-        }
-
-        private void BtnGeneratePwd_Click(object sender, RoutedEventArgs e)
-        {
-            GeneratePwd fmgp = new GeneratePwd();
-            fmgp.ShowDialog();
         }
     }
 }
